ea2032db088207832d1fed088a33bdb66b8ed7b7
gcrowards <gcrowards@lincoln.edu.gh>
2016-10-22 18:00:18 -0700

    Evaluating points
    This little do-dad will evaluate the 3 points to find the best fitting
    line. The evaluatePoints code is the start of a class to analyze
    circular or linear points.

---
+++ evaluatePoints.py
@@ -0,0 +1,21 @@
+import numpy as np
+import scipy
+
+
+class evalPoints(object):
+	def __init__(self, points):
+		# termination criteria
+		self.points = points
+
+	def lineAnalysis(self):
+		# Determines the best fit of a set of 3 points, with the center point
+		dataMean = self.points.mean(axis=0)
+		uu, dd, vv = np.linalg.svd(self.points-dataMean)
+		lineVect = vv[0]
+		error = 0
+		for v in self.points:
+			point2line = np.linalg.norm(np.cross(lineVect,v-dataMean))
+			error = error+point2line
+		return vv[0], dataMean, error
+
+	def circleAnalysis(self):
