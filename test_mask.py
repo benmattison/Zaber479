a81fb36b60e97a4fe6c26dd661e1bc6c0d980082
benmattison <ben.mattison94@gmail.com>
2017-01-11 14:47:18 -0800

    Added test_mask for checking camera/ball tracking parameters

---
+++ test_mask.py
@@ -0,0 +1,22 @@
+import stereoTracking as st
+import usefulFunctions as us
+import calibrateCameras as cb
+
+settingsPath = "settingsLogi.json"
+userSettings = us.readJson(settingsPath)
+Lcam_int = userSettings["Lcam"]
+Rcam_int = userSettings["Rcam"]
+calConstants = cb.loadCalibration(userSettings["calPath"])
+
+print calConstants
+calConstants[8]=(1280,720)
+
+sqSize = userSettings["chessboardSquareSize"]
+track = st.StereoTracker(calConstants, sqSize)  # initialize stereo tracker
+track.initializeCameras(Lcam_int, Rcam_int, Lcam_exposure=-5, Rcam_exposure=-5, fps=30)
+
+
+track.showVideo()
+track.showMask('pink')
+
+track.close()
